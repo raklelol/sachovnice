4ae2c862fa99d7c9f0c0493a0a766820a741c060
Martin poseidon vltavy <swagdudez@eskola.eu>
2023-09-15 11:33:11 +0200

    Funkční halovy sondy a výpis matice, spravit tranzistory

---
+++ sachovnice2/sachovnice2.ino
@@ -0,0 +1,56 @@
+
+#define pin 2 
+#define minus1 3
+#define minus2 4
+#define plus1 5
+#define plus2 6
+int plocha[2][2];
+int plus[2];
+int minus[2];
+void setup() {
+  Serial.begin(9600);
+  pinMode(pin, INPUT);
+  pinMode(minus1, OUTPUT);
+  pinMode(minus2, OUTPUT);
+  pinMode(plus1, OUTPUT);
+  pinMode(plus2, OUTPUT);
+  minus[0] = 2;
+  minus[1] = 3;
+  plus[0] = 5;
+  plus[1] = 6;
+  plocha[0][0] = 1;
+  plocha[0][1] = 1;
+  plocha[1][0] = 1;
+  plocha[1][1] = 1;
+}
+
+void loop() {
+  sachovnice();
+  vypis();
+  delay(1000);
+  
+
+}
+
+void sachovnice() {
+  for (int radek = 0; radek < 2; radek++) {
+    for (int sloupec = 0; sloupec < 2; sloupec++) {
+      digitalWrite(plus[sloupec], HIGH);
+      digitalWrite(minus[radek], HIGH);
+      plocha[sloupec][radek] = digitalRead(pin);
+      digitalWrite(plus[sloupec], LOW);
+      digitalWrite(minus[radek], LOW);
+    }
+  }
+}
+
+void vypis() {
+  for (int radek = 0; radek < 2; radek++) {
+    for (int sloupec = 0; sloupec < 2; sloupec++) {
+     Serial.print(plocha[sloupec][radek]);
+     
+    }
+    Serial.println(" ");
+  }
+  Serial.println(" ");
+}
